d34ecf80c63a035d631fb7856f3147b95df73a42
AyaanG123 <160043446+AyaanG123@users.noreply.github.com>
2024-11-25 09:20:37 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+//Ayaan Gill
+//Pizza Shop Lab
+//11-25-24
+//extra: tax
+#include <iostream>
+#include <iomanip>
+using namespace std;
+//declaring functions
+double calcTotal(int toppings, char size, double toppingsPrice, double sizePrice);
+void bill(double total);
+int main() {
+    int toppings;
+    char size;
+    double toppingsPrice;
+    double sizePrice;
+    double total = calcTotal(toppings, size, toppingsPrice, sizePrice);
+    cout<<"WELCOME TO GILLY PIZZA"<<endl;
+    cout<<"WHAT SIZE PIZZA WOULD YOU LIKE(S OR L): ";
+    cin>>size;
+    cout<<"HOW MANY TOPPINGS WOULD YOU LIKE: ";
+    cin>>toppings;
+    bill(total);//calling function
+
+    return 0;
+}
+//function to calculate total price
+double calcTotal(int toppings, char size, double toppingsPrice, double sizePrice){
+    if(tolower(size) == 's'){
+        sizePrice = 9.99;
+    }
+    else{
+        sizePrice = 14.99;
+    }
+    
+    toppingsPrice = toppings*0.55;
+    
+    return sizePrice + toppingsPrice;
+    
+    
+}
+
+void bill(double total){
+    cout<<"-------GILLY PIZZA-------"<<endl;
+    cout<<"SUBTOTAL: $"<< total<<endl;
+    cout<<"TAX: $"<< fixed << setprecision(2) << total*0.06625<<endl;
+    cout<<"TOTAL: $"<< total*1.06625<<endl;
+    cout<<"--------THANK YOU--------"<<endl;
+}
